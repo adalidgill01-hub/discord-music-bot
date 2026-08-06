821fe8a34805480a647d3a224a1d2bf4d8004fc9
adalidgill01-hub <adalidgill01@gmail.com>
2026-08-06 02:43:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -24,12 +24,27 @@ client.on("messageCreate", async (message) => {
   }
 
   joinVoiceChannel({
-    channelId: message.member.voice.channel.id,
-    guildId: message.guild.id,
-    adapterCreator: message.guild.voiceAdapterCreator,
-  });
+  channelId: message.member.voice.channel.id,
+  guildId: message.guild.id,
+  adapterCreator: message.guild.voiceAdapterCreator,
+  selfDeaf: false,
+  selfMute: false,
+});
 
   message.reply("✅ Entré al canal de voz.");
 });
+setInterval(() => {
+  const canal = client.channels.cache.get("1534842015307534366");
+
+  if (canal) {
+    joinVoiceChannel({
+      channelId: canal.id,
+      guildId: canal.guild.id,
+      adapterCreator: canal.guild.voiceAdapterCreator,
+      selfDeaf: false,
+      selfMute: false,
+    });
+  }
+}, 60000);
 
 client.login(process.env.TOKEN);
